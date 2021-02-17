661172669987602bfbff96a9a934a01b8614fd49
Michael Popp <michael.christopher.popp@gmail.com>
2021-02-17 16:38:04 -0600

    another commit, little progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 function App() {
